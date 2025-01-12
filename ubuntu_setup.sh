561a0acde06a4b31934872dc49367d521e522b7a
Gabriel <gabrielkrodrigues1@gmail.com>
2025-01-12 12:01:46 -0300

    add script

---
+++ ubuntu_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# GIT configs & SSH
+git config --global user.email "gabrielkrodrigues1@gmail.com"
+git config --global user.name "Gabriel"
+ssh-keygen -t ed25519 -C "gabrielkrodrigues1@gmail.com"
+
+# Install prerequisites for PHP and add PHP repository
+sudo apt -y install software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+
+# Install PHP 8.3 and PHP-FPM 8.3
+sudo apt -y install php8.3 php8.3-fpm
+
+# Install PHP extensions for PHP 8.3
+sudo apt -y install php8.3-pdo php8.3-mysql php8.3-mbstring php8.3-exif php8.3-gd
+
+# Install Composer
+sudo apt -y install curl php-cli unzip
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+source ~/.bashrc
+
+# Install Node.js using NVM
+nvm install node
